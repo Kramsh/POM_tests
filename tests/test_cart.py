794700c741461dd65bf49474977a9ed7363bb084
Kramsh <sergeroq@gmail.com>
2025-12-27 01:39:58 +0300

    fix

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,31 +1,31 @@
-# from pages.cart_page import CartPage
+from pages.cart_page import CartPage
 
 
-# def test_cart_is_empty_by_default(driver):
-#     cart_page = CartPage(driver)
-#     cart_page.open_cart()
+def test_cart_is_empty_by_default(driver):
+    cart_page = CartPage(driver)
+    cart_page.open_cart()
 
-#     assert cart_page.is_cart_empty()
-
-
-# def test_search_in_cart(driver):
-#     cart_page = CartPage(driver)
-#     cart_page.open_cart()
-#     cart_page.search_product("Corner Desk Left Sit")
-#     cart_page.click_first_search_result()
-#     expected_url = 'http://testshop.qa-practice.com/shop/furn-1118-corner-desk-left-sit-18'
-#     actual_url = cart_page.get_current_url()
-
-#     assert actual_url == expected_url
+    assert cart_page.is_cart_empty()
 
 
-# def test_check_items_in_categories_dropdown_in_cart(driver):
-#     cart_page = CartPage(driver)
-#     cart_page.open_cart()
+def test_search_in_cart(driver):
+    cart_page = CartPage(driver)
+    cart_page.open_cart()
+    cart_page.search_product("Corner Desk Left Sit")
+    cart_page.click_first_search_result()
+    expected_url = 'http://testshop.qa-practice.com/shop/furn-1118-corner-desk-left-sit-18'
+    actual_url = cart_page.get_current_url()
 
-#     menu_count = cart_page.get_dropdown_menu_count()
-#     assert menu_count == 2
+    assert actual_url == expected_url
 
-#     menu_texts = cart_page.get_dropdown_menu_texts()
-#     expected_texts = ["Desks", "Furnitures"]
-#     assert menu_texts == expected_texts
+
+def test_check_items_in_categories_dropdown_in_cart(driver):
+    cart_page = CartPage(driver)
+    cart_page.open_cart()
+
+    menu_count = cart_page.get_dropdown_menu_count()
+    assert menu_count == 2
+
+    menu_texts = cart_page.get_dropdown_menu_texts()
+    expected_texts = ["Desks", "Furnitures"]
+    assert menu_texts == expected_texts
